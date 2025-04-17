398babbb137b79deffc5ee8562f89d24c0ec557e
Gabriel Correia <camparicorreia@gmail.com>
2025-04-17 16:28:54 -0300

    feat: buy alerts and track buy function

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-function comprar() {}
+function buyTicket() {
+  let ticketQuantity = parseInt(document.getElementById("qtd").value);
+  let ticketType = document.getElementById("tipo-ingresso");
+
+  if (ticketType.value == "pista") {
+    comprarPista(ticketQuantity);
+  }
+}
+
+function comprarPista(ticketQuantity) {
+  let pistaQuantity = parseInt(
+    document.getElementById("qtd-pista").textContent
+  );
+
+  if (ticketQuantity > pistaQuantity) {
+    alert("Quantidade para tipo pista indisponível no momento!");
+  } else {
+    pistaQuantity -= ticketQuantity;
+    document.getElementById("qtd-pista").textContent = pistaQuantity;
+    alert("Compra realizada com sucesso!");
+  }
+}
